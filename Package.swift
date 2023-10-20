3e475224d9b6847a658f2eba0a7d5cf993d9877e
marto171 <georgievmarti21@gmail.com>
2023-10-20 19:18:26 +0300

    remove trash comments

--- Package.swift
+++ Package.swift
@@ -6,14 +6,11 @@ import PackageDescription
 let package = Package(
     name: "iOS-Backend-SDK",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "iOS-Backend-SDK",
             targets: ["iOS-Backend-SDK"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "iOS-Backend-SDK"),
         .testTarget(
