8e6eea46ba2135fda0b653348c1cb5b42b20e120
marto171 <georgievmarti21@gmail.com>
2023-10-21 17:31:34 +0300

    add comment on the tests

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -4,7 +4,7 @@ import XCTest
 @MainActor
 final class iOS_Backend_SDKTests: XCTestCase {
     func testExample() async throws {
-        let result = await Backend.main.login(config: ExampleConfig, body: LoginRequest(email: "georgievmarti21@gmail.com", password: "Az@dert123gg"))
+//        let result = await Backend.main.login(config: ExampleConfig, body: LoginRequest(email: "georgievmarti21@gmail.com", password: "Az@dert123gg"))
         
     }
 }
