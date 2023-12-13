d6d0b49b6e29757ba79a70874cb0dcb57cd98402
ikok07 <kokmarok@gmail.com>
2023-12-13 21:25:07 +0200

    make getUserDetails method public

--- Sources/Backend Methods/GetUserDetails.swift
+++ Sources/Backend Methods/GetUserDetails.swift
@@ -10,7 +10,7 @@ import NetworkRequests
 
 extension Backend {
     
-    func getUserDetails(userId: String, callback: (Result<GetUserDataResponse, BackendError<String>>) -> Void) async {
+    public func getUserDetails(userId: String, callback: (Result<GetUserDataResponse, BackendError<String>>) -> Void) async {
         
         guard let config = self.config else {
             callback(.failure(K.SDKError.noConfigError))
