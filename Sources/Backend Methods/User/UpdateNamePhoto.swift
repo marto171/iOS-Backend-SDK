2e6a71b27dc9d081e1418367d64068aa3540d6cd
ikok07 <kokmarok@gmail.com>
2023-12-18 10:30:08 +0200

    make callback of updateNamePhoto async

--- Sources/Backend Methods/User/UpdateNamePhoto.swift
+++ Sources/Backend Methods/User/UpdateNamePhoto.swift
@@ -10,16 +10,16 @@ import UIKit
 import NetworkRequests
 
 extension Backend {
-    public func updateNamePhoto(name: String, image: UIImage?, authToken: String, callback: (Result<ProfileDataResponse, BackendError<String>>) -> Void) async {
+    public func updateNamePhoto(name: String, image: UIImage?, authToken: String, callback: (Result<ProfileDataResponse, BackendError<String>>)  async -> Void) async {
         guard let config = self.config else {
-            callback(.failure(K.SDKError.noConfigError))
+            await callback(.failure(K.SDKError.noConfigError))
             return
         }
         
         let request: Result<ProfileDataResponse, NetworkError>? = await Request.patchFormData(url: "\(config.baseUrl)/\(config.language)/api/v1/user/me", json: name.data(using: .utf8)!, images: [image], authToken: authToken)
         
         guard let request else {
-            callback(.failure(K.SDKError.noAPIConnectionError))
+            await callback(.failure(K.SDKError.noAPIConnectionError))
             return
         }
         
@@ -27,13 +27,13 @@ extension Backend {
         case .success(let response):
             switch response.status {
             case "success":
-                return callback(.success(response));
+                return await callback(.success(response));
             default:
                 callback(.failure(config.getError(BackendErrorType(rawValue: response.identifier ?? "")) ?? BackendError(type: .Custom, localizedDescription: response.message ?? K.SDKMessage.genericMessage)))
             }
         case .failure(let error):
             print("Form data error: \(error)")
-            callback(.failure(K.SDKError.noAPIConnectionError))
+            await callback(.failure(K.SDKError.noAPIConnectionError))
         }
         
     }
