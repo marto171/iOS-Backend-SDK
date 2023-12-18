0b86ae1345c05eb4b422774822fb3ef83ab2cedf
ikok07 <kokmarok@gmail.com>
2023-12-18 22:46:05 +0200

    add auth token to getUserDetails()

--- Sources/Backend Methods/User/GetUserDetails.swift
+++ Sources/Backend Methods/User/GetUserDetails.swift
@@ -10,14 +10,14 @@ import NetworkRequests
 
 extension Backend {
     
-    public func getUserDetails(userId: String, callback: (Result<GetUserDetailsResponse, BackendError<String>>) async -> Void) async {
+    public func getUserDetails(userId: String, authToken: String, callback: (Result<GetUserDetailsResponse, BackendError<String>>) async -> Void) async {
         
         guard let config = self.config else {
             await callback(.failure(K.SDKError.noConfigError))
             return
         }
         
-        let request: Result<GetUserDetailsResponse, NetworkError> = await Request.get(url: "\(config.baseUrl)/\(config.language)/api/v1/user/\(userId)/details")
+        let request: Result<GetUserDetailsResponse, NetworkError> = await Request.get(url: "\(config.baseUrl)/\(config.language)/api/v1/user/\(userId)/details", authToken: authToken)
         
         switch request {
         case .success(let response):
