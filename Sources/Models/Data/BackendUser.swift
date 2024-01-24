144cd37bec91bd4c479f00031b3aab63e8846818
ikok07 <kokmarok@gmail.com>
2024-01-24 18:32:34 +0200

    make BackendUser's init public

--- Sources/Models/Data/BackendUser.swift
+++ Sources/Models/Data/BackendUser.swift
@@ -15,4 +15,14 @@ public class BackendUser: Codable {
     public var email: String
     public var photo: String
     public var oauthProvider: String
+    
+    public init(_id: String, oauthProviderUserId: String? = nil, token: String? = nil, name: String, email: String, photo: String, oauthProvider: String) {
+        self._id = _id
+        self.oauthProviderUserId = oauthProviderUserId
+        self.token = token
+        self.name = name
+        self.email = email
+        self.photo = photo
+        self.oauthProvider = oauthProvider
+    }
 }
